ad01b9dcfcfd3b27d527a49e6a2f11e3eb5fc611
CutoNaito <loliknightowo@gmail.com>
2023-02-09 20:05:27 +0100

    Small fix that did kinda nothing

--- src/commands/CommandController.ts
+++ src/commands/CommandController.ts
@@ -1,20 +1,16 @@
 import { Ping, TRANSLATELOCL } from "./TranslateCommands"
 
 export function CommandController(data: string) {
-    try {
-        if (data === "ping") {
-            return Ping()
-        }
+    if (data === "ping") {
+        return Ping()
+    }
 
-        if (data.includes("TRANSLATELOCL")) {
-            try {
-                const word = data.split("\"")[1];
-                return TRANSLATELOCL(word);
-            } catch (error) {
-                return 'TRANSLATEDERR"Word not found"';
-            }
+    if (data.includes("TRANSLATELOCL")) {
+        try {
+            const word = data.split("\"")[1];
+            return TRANSLATELOCL(word);
+        } catch (error) {
+            return 'TRANSLATEDERR"Word not found"';
         }
-    } catch (error) {
-        return "Error: This command does not exist";
     }
 }
